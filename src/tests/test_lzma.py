b3c39a2e9d1273929200d4f307f4c23fe3f75b82
benny <benny.colyn@gmail.com>
2023-01-27 13:55:41 +0100

    fix test

--- src/tests/test_lzma.py
+++ src/tests/test_lzma.py
@@ -1,11 +1,20 @@
 import lzma
+from pathlib import Path
+
 from astropy.io import fits
 from astropy.io.fits import HDUList
 
 
 def test_read_lzma_header():
-    filename = r'E:\Astro_Archief\Deep Sky\Raw\ZWO_ASI183MM_Pro\2019-03-29\Light\Medusa_2019-03-29T212651_120sec_SII__-25C_frame8.fit.xz'
+    filename = resolve_test_data('test_image.fits.xz')
     with lzma.open(filename) as f:
         hdul: HDUList = fits.open(f)
         hdul.info()
         print(repr(hdul[0].header))
+
+
+def resolve_test_data(file):
+    root = Path(__file__).parent
+    path = Path(root, "test-data", file)
+    assert path.exists()
+    return path
